7a86e487015ed6d2fdee52e3ee5977b64486e7a8
bnmnjohnson <njohnson@brandnewmedia.com.au>
2016-03-03 10:12:08 +1100

    changed to tpl files and added metadataservice

---
+++ app/js/services/MetadataService.js
@@ -0,0 +1,25 @@
+function MetadataService() {
+
+    var title,
+        description,
+        defaultTitle = '%DEFAULT_TITLE%',
+        defaultDescription = '%DEFAULT_DESCRIPTION%';
+
+    this.setMetadata = function(metadata) {
+        title = metadata.title ? metadata.title : defaultTitle;
+        description = metadata.description ? metadata.description : defaultDescription;
+    };
+
+    this.getMetadata = function() {
+        return {
+            title: title,
+            description: description
+        };
+    };
+}
+
+
+export default {
+    name: 'MetadataService',
+    fn: MetadataService
+};
